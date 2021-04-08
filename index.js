f33c16c12823f19206c7a5281ff99ffd97d3d734
Brinton <brintonfoyreed@gmail.com>
2021-04-08 06:27:14 -0700

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts); 
+
+let output = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++ ) {
+        output.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return output;
+}
+
+
+function countDown(num){
+    while (num > -1) {
+    console.log(num);
+    num--;
+}}
